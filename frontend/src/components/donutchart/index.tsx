15ff2d836d03ebeebdc9a48e5740c5f1d718a28e
heitorbrunini <68927093+heitorbrunini@users.noreply.github.com>
2021-11-09 19:07:30 -0300

    First request

--- frontend/src/components/donutchart/index.tsx
+++ frontend/src/components/donutchart/index.tsx
@@ -1,12 +1,29 @@
+import axios from "axios";
 import Chart from "react-apexcharts";
+import { SaleSum } from "types/sale";
+import { BASE_URL } from "utils/requests";
+
+type ChartData={
+    labels: string[];
+    series: number[];
+}
 
 const DonutChart = () => {
+    let chartData: ChartData= { labels: [], series: []}
 
-    const mockData = {
+    axios.get(BASE_URL + '/sales/sum')
+        .then( response =>{
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+            chartData= {labels: myLabels, series: mySeries};
+            console.log();
+    });
+/*  const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+*/
     const options = {
         legend: {
             show: true
@@ -14,8 +31,8 @@ const DonutChart = () => {
     }
     return (
         <Chart 
-            options={{...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{...options, labels: chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="240"
         />
