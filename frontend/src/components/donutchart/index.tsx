d9e167209281155737bba681452a411fc5ea68bc
heitorbrunini <68927093+heitorbrunini@users.noreply.github.com>
2021-11-09 19:28:47 -0300

    Donutchart integration

--- frontend/src/components/donutchart/index.tsx
+++ frontend/src/components/donutchart/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { SaleSum } from "types/sale";
 import { BASE_URL } from "utils/requests";
@@ -9,16 +10,19 @@ type ChartData={
 }
 
 const DonutChart = () => {
-    let chartData: ChartData= { labels: [], series: []}
 
-    axios.get(BASE_URL + '/sales/sum')
+    const [chartData, setChartData] = useState<ChartData>({labels: [], series: []});
+
+    useEffect(()=> {
+        axios.get(BASE_URL + '/sales/sum')
         .then( response =>{
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
-            chartData= {labels: myLabels, series: mySeries};
-            console.log();
-    });
+            setChartData( {labels: myLabels, series: mySeries} );
+        });
+    },[] );
+    
 /*  const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
@@ -40,4 +44,12 @@ const DonutChart = () => {
 }
 
 export default DonutChart;
+
+function labels<T>(labels: any, arg1: never[], series: any, arg3: never[]): [any, any] {
+    throw new Error("Function not implemented.");
+}
+
+function series<T>(labels: <T>(labels: any, arg1: never[], series: any, arg3: never[]) => [any, any], arg1: never[], series: any, arg3: never[]): [any, any] {
+    throw new Error("Function not implemented.");
+}
 
