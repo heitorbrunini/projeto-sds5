646b47d1ae6a0eca07581fb38e9cad312fe88659
heitorbrunini <68927093+heitorbrunini@users.noreply.github.com>
2021-11-09 22:30:37 -0300

    DataTable integration

--- frontend/src/components/datatable/index.tsx
+++ frontend/src/components/datatable/index.tsx
@@ -1,4 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
 const DataTable = () => {
+    const [page, setPage] = useState<SalePage>({
+        last: true,
+        totalElements: 0,
+        totalPages: 0,
+        number: 0,
+        first: true,
+        numberOfElements: 0
+    });
+
+    useEffect(() => {
+        axios.get(BASE_URL + "/sales?page=2&size=10&sort=date")
+            .then(response => {
+                setPage(response.data);
+            })
+    }, []);
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -12,48 +33,17 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
+                    {page.content?.map(item => (
+                        <tr key ={item.id}>
+                            <td>{formatLocalDate(item.date,"dd/MM/yyyy")}</td>
+                            <td>{item.seller.name}</td>
+                            <td>{item.visited}</td>
+                            <td>{item.deals}</td>
+                            <td>{item.amount.toFixed(2)}</td>
+                        </tr>
+                    ))}
+
+
                 </tbody>
             </table>
         </div>
---
+++ frontend/src/types/seller.ts
@@ -0,0 +1,5 @@
+export type Seller = {
+    id: number;
+    name: string;
+}
+
